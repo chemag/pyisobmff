66101d8ce99a5ad7fdb8328b48ddacd00971f30f
Chema Gonzalez <chemag@gmail.com>
2023-04-12 13:48:47 -0700

    pyisobmff: make test script usable
    Tested:
    ```
    $ ./scripts/isobmff-parse.py media/C001.heic
    ...
    mdat: 1080
    mdat: 112700
    mdat: 1005803
    ISOBaseMediaFile
      <isobmff.ftyp.FileTypeBox object at 0x7f65c0998790>  major_brand: heic
        minor_version: 0
        compatible_brands: msf1,mif1,heic,hevc,iso8,
      <isobmff.meta.MetaBox object at 0x7f65c09dc5d0>
      <isobmff.moov.MovieBox object at 0x7f65c09dc550>
      <isobmff.mdat.MediaDataBox object at 0x7f65c09dca50>
      <isobmff.mdat.MediaDataBox object at 0x7f65c0a03010>
      <isobmff.mdat.MediaDataBox object at 0x7f65c09dce50>
    ```

--- scripts/isobmff-parse.py
+++ scripts/isobmff-parse.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""isobmff-parse.py: A generic ISOBMFF (ISO base media file,
+ISO/IEC 14496-12) parser."""
 
+import argparse
 import os
 import sys
 
@@ -11,9 +14,96 @@ sys.path.append(rootdir)
 
 import isobmff
 
+__version__ = "0.1"
 
-TESTFILE = os.path.join(rootdir, "media/C001.heic")
-if __name__ == "__main__":
+default_values = {
+    "debug": 0,
+    "dry_run": False,
+    "infile": None,
+}
+
+
+def parse_file(infile, debug):
     media_file = isobmff.MediaFile()
-    media_file.read(TESTFILE)
-    # print(media_file)
+    media_file.read(infile)
+    print(media_file)
+
+
+def get_options(argv):
+    """Generic option parser.
+
+    Args:
+        argv: list containing arguments
+
+    Returns:
+        Namespace - An argparse.ArgumentParser-generated option object
+    """
+    # init parser
+    # usage = 'usage: %prog [options] arg1 arg2'
+    # parser = argparse.OptionParser(usage=usage)
+    # parser.print_help() to get argparse.usage (large help)
+    # parser.print_usage() to get argparse.usage (just usage line)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        dest="version",
+        default=False,
+        help="Print version",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="count",
+        dest="debug",
+        default=default_values["debug"],
+        help="Increase verbosity (use multiple times for more)",
+    )
+    parser.add_argument(
+        "--quiet",
+        action="store_const",
+        dest="debug",
+        const=-1,
+        help="Zero verbosity",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        dest="dry_run",
+        default=default_values["dry_run"],
+        help="Dry run",
+    )
+    parser.add_argument(
+        "infile",
+        type=str,
+        nargs="?",
+        default=default_values["infile"],
+        metavar="input-file",
+        help="input file",
+    )
+    # do the parsing
+    options = parser.parse_args(argv[1:])
+    if options.version:
+        return options
+    # implement help
+    # parser.print_help()
+    # sys.exit(0)
+    return options
+
+
+def main(argv):
+    # parse options
+    options = get_options(argv)
+    if options.version:
+        print("version: %s" % __version__)
+        sys.exit(0)
+    if options.debug > 0:
+        print(options)
+    # parse the input file
+    parse_file(options.infile, options.debug)
+
+
+if __name__ == "__main__":
+    # at least the CLI program name: (CLI) execution
+    main(sys.argv)
