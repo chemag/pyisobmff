326d9a0d5678c8eb0b5b642cee1d8df64c89a636
Chema Gonzalez <chemag@gmail.com>
2023-04-14 09:59:31 -0700

    pyisobmff: simplify mdat box
    Avoid reading the contents (we are not doing anything with the mdat
    box contents).

--- isobmff/mdat.py
+++ isobmff/mdat.py
@@ -6,13 +6,6 @@ class MediaDataBox(Box):
     box_type = "mdat"
     is_mandatory = False
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.data_offset = None
-
     def read(self, file):
-        # store the offset
-        self.data_offset = file.tell()
-        offset = file.tell()
         max_offset = self.get_max_offset()
-        file.read(max_offset - offset)
+        file.seek(max_offset)
