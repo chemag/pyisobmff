07dea519ccc44819fe86d9fbd0d968e028d5b06a
Chema Gonzalez <chemag@gmail.com>
2023-04-13 18:40:54 -0700

    pyisobmff: fix bug on hdlr box reading
    Make sure we always read the whole box.

--- isobmff/hdlr.py
+++ isobmff/hdlr.py
@@ -4,6 +4,7 @@ from .box import Quantity
 from .box import read_uint
 from .box import read_fixed_size_string
 from .box import read_utf8string
+from .box import read_bytes
 
 
 # ISO/IEC 14496-12:2022, Section 8.4.3
@@ -14,10 +15,6 @@ class HandlerReferenceBox(FullBox):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.pre_defined = None
-        self.handler_type = None
-        self.reserved = []
-        self.name = None
 
     def __repr__(self):
         repl = ()
@@ -28,7 +25,11 @@ class HandlerReferenceBox(FullBox):
     def read(self, file):
         self.pre_defined = read_uint(file, 4)
         self.handler_type = read_fixed_size_string(file, 4)
+        self.reserved = []
         for _ in range(3):  # 3*4=12bytes
             self.reserved.append(read_uint(file, 4))
         max_len = self.get_max_offset() - file.tell()
         self.name = read_utf8string(file, max_len)
+        # ensure we read all the way to the end of the box
+        max_len = self.get_max_offset() - file.tell()
+        garbage = read_bytes(file, max_len)
