b4066905a449c58778d3bf10aea1127f4a45ff93
Chema Gonzalez <chemag@gmail.com>
2023-04-14 16:12:38 -0700

    pyisobmff: add mp4s box

--- isobmff/mp4.py
+++ isobmff/mp4.py
@@ -5,6 +5,7 @@ from .box import read_box
 from .box import read_uint
 from .stbl import VisualSampleEntry
 from .stbl import AudioSampleEntry
+from .stbl import SampleEntry
 
 
 # ISO/IEC 14496-14:2020, Section 6.7.2
@@ -29,6 +30,20 @@ class MP4VisualSampleEntry(VisualSampleEntry):
 # ISO/IEC 14496-14:2020, Section 6.7.2
 class MP4AudioSampleEntry(AudioSampleEntry):
     box_type = "mp4a"
+
+    def read(self, file):
+        super().read(file)
+        self.ES = read_box(file, self.debug)
+
+    def __repr__(self):
+        repl = ()
+        repl += (f"ES: {self.ES}",)
+        return super().repr(repl)
+
+
+# ISO/IEC 14496-14:2020, Section 6.7.2
+class MpegSampleEntry(SampleEntry):
+    box_type = "mp4s"
 
     def read(self, file):
         super().read(file)
