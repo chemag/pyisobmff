c38f550c95128076cbdcdd99151c9165c6778c25
Chema Gonzalez <chemag@gmail.com>
2023-04-14 13:38:26 -0700

    pyisobmff: simplify skip implementation

--- isobmff/free.py
+++ isobmff/free.py
@@ -17,15 +17,5 @@ class FreeBox(Box):
 
 
 # ISO/IEC 14496-12:2022, Section 8.1.2
-class SkipBox(Box):
+class SkipBox(FreeBox):
     box_type = "skip"
-    is_mandatory = False
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.contents = None
-
-    def __repr__(self):
-        repl = ()
-        repl += (f'contents: "{self.contents}"',)
-        return super().repr(repl)
