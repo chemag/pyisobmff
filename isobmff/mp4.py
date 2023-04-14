44ddd8ff9536511661a6ecc56690ad302cc9a47e
Chema Gonzalez <chemag@gmail.com>
2023-04-14 16:12:14 -0700

    pyisobmff: add mp4a box

--- isobmff/mp4.py
+++ isobmff/mp4.py
@@ -4,6 +4,7 @@ from .box import Quantity
 from .box import read_box
 from .box import read_uint
 from .stbl import VisualSampleEntry
+from .stbl import AudioSampleEntry
 
 
 # ISO/IEC 14496-14:2020, Section 6.7.2
@@ -14,6 +15,20 @@ class EDSBox(FullBox):
 # ISO/IEC 14496-14:2020, Section 6.7.2
 class MP4VisualSampleEntry(VisualSampleEntry):
     box_type = "mp4v"
+
+    def read(self, file):
+        super().read(file)
+        self.ES = read_box(file, self.debug)
+
+    def __repr__(self):
+        repl = ()
+        repl += (f"ES: {self.ES}",)
+        return super().repr(repl)
+
+
+# ISO/IEC 14496-14:2020, Section 6.7.2
+class MP4AudioSampleEntry(AudioSampleEntry):
+    box_type = "mp4a"
 
     def read(self, file):
         super().read(file)
