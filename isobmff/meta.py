62d6a90cba2c3250f096fa5ae89f0255878972b9
Chema Gonzalez <chemag@gmail.com>
2023-04-14 19:37:03 -0700

    pyisobmff: sort meta class methods

--- isobmff/meta.py
+++ isobmff/meta.py
@@ -10,13 +10,13 @@ class MetaBox(FullBox):
     quantity = Quantity.ZERO_OR_ONE
     box_list = []
 
+    def read(self, file):
+        while file.tell() < self.get_max_offset():
+            box = read_box(file, self.debug)
+            self.box_list.append(box)
+
     def __repr__(self):
         repl = ()
         for box in self.box_list:
             repl += (repr(box),)
         return super().repr(repl)
-
-    def read(self, file):
-        while file.tell() < self.get_max_offset():
-            box = read_box(file, self.debug)
-            self.box_list.append(box)
