5a1bed0932110fa94926be5c242fadfe4e7740a3
Chema Gonzalez <chemag@gmail.com>
2023-04-14 18:25:04 -0700

    pyisobmff: complete sidx box

--- isobmff/sidx.py
+++ isobmff/sidx.py
@@ -9,6 +9,8 @@ class SegmentIndexBox(FullBox):
     references = []
 
     def read(self, file):
+        self.reference_ID = read_uint(file, 4)
+        self.timescale = read_uint(file, 4)
         count_size = 4 if self.version == 0 else 8
         self.earliest_presentation_time = read_uint(file, count_size)
         self.first_offset = read_uint(file, count_size)
@@ -17,12 +19,12 @@ class SegmentIndexBox(FullBox):
         for _ in range(reference_count):
             reference = {}
             word1 = read_uint(file, 4)
-            reference.reference_type = word1 >> 31
-            reference.reference_size = word1 & 0x7FFFFFFF
+            reference["reference_type"] = word1 >> 31
+            reference["reference_size"] = word1 & 0x7FFFFFFF
             word2 = read_uint(file, 4)
-            reference.starts_with_SAP = word2 >> 31
-            reference.SAP_type = (word2 >> 28) & 0x7
-            reference.SAP_delta_time = word2 & 0x0FFFFFFF
+            reference["starts_with_SAP"] = word2 >> 31
+            reference["SAP_type"] = (word2 >> 28) & 0x7
+            reference["SAP_delta_time"] = word2 & 0x0FFFFFFF
             self.references.append(reference)
         # skip the remaining data
         max_offset = self.get_max_offset()
@@ -30,13 +32,15 @@ class SegmentIndexBox(FullBox):
 
     def __repr__(self):
         repl = ()
+        repl += (f"reference_ID: {self.reference_ID}",)
+        repl += (f"timescale: {self.timescale}",)
         repl += (f"earliest_presentation_time: {self.earliest_presentation_time}",)
         repl += (f"first_offset: {self.first_offset}",)
         repl += (f"reserved: {self.reserved}",)
-        for idx, val in self.references:
-            repl += (f"reference[{idx}].reference_type: {val.reference_type}",)
-            repl += (f"reference[{idx}].reference_size: {val.reference_size}",)
-            repl += (f"reference[{idx}].starts_with_SAP: {val.starts_with_SAP}",)
-            repl += (f"reference[{idx}].SAP_type: {val.SAP_type}",)
-            repl += (f"reference[{idx}].SAP_delta_time: {val.SAP_delta_time}",)
+        for idx, val in enumerate(self.references):
+            repl += (f'reference[{idx}].reference_type: {val["reference_type"]}',)
+            repl += (f'reference[{idx}].reference_size: {val["reference_size"]}',)
+            repl += (f'reference[{idx}].starts_with_SAP: {val["starts_with_SAP"]}',)
+            repl += (f'reference[{idx}].SAP_type: {val["SAP_type"]}',)
+            repl += (f'reference[{idx}].SAP_delta_time: {val["SAP_delta_time"]}',)
         return super().repr(repl)
