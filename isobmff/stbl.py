d39be2bc85c7c998e038b15a9ae12b3607388983
Chema Gonzalez <chemag@gmail.com>
2023-04-17 17:25:34 -0700

    pyisobmff: add support for stpp boxes

--- isobmff/stbl.py
+++ isobmff/stbl.py
@@ -224,3 +224,24 @@ class TextMetaDataSampleEntry(MetaDataSampleEntry):
         for box in self.box_list:
             repl += (repr(box),)
         return super().repr(repl)
+
+
+# ISO/IEC 14496-12:2022, Section 12.3.3.2
+class XMLSubtitleSampleEntry(SubtitleSampleEntry):
+    box_type = b"stpp"
+
+    def read(self, file):
+        super().read(file)
+        max_len = self.get_max_offset() - file.tell()
+        self.namespace = read_utf8string(file, max_len)
+        max_len = self.get_max_offset() - file.tell()
+        self.schema_location = read_utf8string(file, max_len)
+        max_len = self.get_max_offset() - file.tell()
+        self.auxiliary_mime_types = read_utf8string(file, max_len)
+
+    def __repr__(self):
+        repl = ()
+        repl += (f"namespace: {self.namespace}",)
+        repl += (f"schema_location: {self.schema_location}",)
+        repl += (f"auxiliary_mime_types: {self.auxiliary_mime_types}",)
+        return super().repr(repl)
