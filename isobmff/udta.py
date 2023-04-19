a8e51ef5f41a1efae67faa33febca277508c6193
Chema Gonzalez <chemag@gmail.com>
2023-04-19 08:46:22 -0700

    pyisobmff: make udta a simple Box
    While the standard (ISO/IEC 14496-12:2022, Section 8.10) describes
    it as a container ("This box contains objects that declare user
    information about the containing box and its data (presentation or
    track)"), there are too many examples where the contents seem to
    be not boxes.

--- isobmff/udta.py
+++ isobmff/udta.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from .box import ContainerBox
+from .box import Box
 
 
 # ISO/IEC 14496-12:2022, Section 8.10
-class UserDataBox(ContainerBox):
+class UserDataBox(Box):
     box_type = b"udta"
