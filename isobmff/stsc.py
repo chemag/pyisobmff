508332df2e350e5b38bcb1baaad364425ea60177
Chema Gonzalez <chemag@gmail.com>
2023-04-18 17:33:46 -0700

    pyisobmff: ensure stsc boxes are read in full

--- isobmff/stsc.py
+++ isobmff/stsc.py
@@ -2,6 +2,7 @@
 from .box import FullBox
 from .box import Quantity
 from .box import read_uint
+from .box import read_bytes
 
 
 # ISO/IEC 14496-12:2022, Section 8.7.4
@@ -19,3 +20,6 @@ class SampleToChunkBox(FullBox):
             entry["samples_per_chunk"] = read_uint(file, 4)
             entry["sample_description_index"] = read_uint(file, 4)
             self.entries.append(entry)
+        # read the rest of the box
+        max_len = self.get_max_offset() - file.tell()
+        self.remaining = read_bytes(file, max_len)
