450f8549fc16c763707c1ba7c73f305700489d58
Chema Gonzalez <chemag@gmail.com>
2023-05-18 10:10:00 -0700

    pyisobmff: turn udta box into container
    Per the definition in the standard.

--- isobmff/udta.py
+++ isobmff/udta.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-from .box import Box
+from .box import ContainerBox
 
 
 # ISO/IEC 14496-12:2022, Section 8.10
-class UserDataBox(Box):
+# "The User Data Box is a container box for informative user-data.
+# This user data is formatted as a set of boxes with more specific
+# box types, which declare more precisely their content. The
+# contained boxes are normal boxes, using a defined, registered,
+# or UUID extension box type."
+class UserDataBox(ContainerBox):
     box_type = b"udta"
