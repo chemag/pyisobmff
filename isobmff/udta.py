c85e5e7681b75706ee2fec856ccd0a4842358203
Chema Gonzalez <chemag@meta.com>
2025-04-11 07:54:16 -0700

    pyisobmff: add support for "cprt" boxes
    Tested:
    ```
    $ isobmff-parse.py -i in.mp4
    ...
        path: /moov/udta
          offset: 0x008ce2f5
          box_type: b'udta'
          size: 162
          path: /moov/udta/cprt
            offset: 0x008ce2fd
            box_type: b'cprt'
            size: 24
            version: 0
            flags: 0
            language: eng
            notice: Notice me\x00
    ...
    ```

--- isobmff/udta.py
+++ isobmff/udta.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from .box import ContainerBox
+from .box import FullBox
+from .utils import read_uint
+from .utils import read_utf8string
 
 
 # ISO/IEC 14496-12:2022, Section 8.10
@@ -10,3 +13,28 @@ from .box import ContainerBox
 # or UUID extension box type."
 class UserDataBox(ContainerBox):
     box_type = b"udta"
+
+
+# ISO/IEC 14496-12:2022, Section 8.10.2
+class CopyrightBox(FullBox):
+    box_type = b"cprt"
+
+    def read(self, file):
+        first_word = read_uint(file, 2)
+        # unsigned int(5)[3] language; // ISO-639-2/T language code
+        # "Each character is packed as the difference between its
+        # ASCII value and 0x60."
+        self.language = (
+            chr(0x60 + ((first_word >> 10) & 0x1F))
+            + chr(0x60 + ((first_word >> 5) & 0x1F))
+            + chr(0x60 + ((first_word >> 0) & 0x1F))
+        )
+        # utfstring notice;
+        max_len = self.max_offset - file.tell()
+        self.notice = read_utf8string(file, max_len)
+
+    def contents(self):
+        tuples = super().contents()
+        tuples += (("language", self.language),)
+        tuples += (("notice", self.notice),)
+        return tuples
